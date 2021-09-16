b97fcd0494965af0cd5af49705e88b9131c8ebdf
Ryan Ramshaw <ramshaw.ryan@outlook.com>
2021-09-16 12:56:50 +0100

    Add Loop to the main menu

--- main.py
+++ main.py
@@ -13,19 +13,20 @@ body_data = [
 
 # Build the ASCII Table based on the main menu data
 body = AsciiTable(body_data)
-print(body.table)
 
-# Listen for user input
-# TODO: add user input cleaning
-selection = input("menu>")
+while True:
+    print(body.table)
+    # Listen for user input
+    # TODO: add user input cleaning
+    selection = input("menu>")
 
-# Check if the input matches a specific pattern
-# TODO: add an else case
-if selection == "1":    
-    kanatest(1)
-elif selection == "2":
-    kanatest(2)
-elif selection == "3":
-    kanatest(0)
-elif selection.upper() == "Q":
-    quit()
+    # Check if the input matches a specific pattern
+    # TODO: add an else case
+    if selection == "1":    
+        kanatest(1)
+    elif selection == "2":
+        kanatest(2)
+    elif selection == "3":
+        kanatest(0)
+    elif selection.upper() == "Q":
+        quit()
